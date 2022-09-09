9fbb97aa12377ec6b2ade73f3e2879051093fb29
ittehadzihan <zihan991@gmail.com>
2022-09-09 11:17:45 -0400

    DONE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+ document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+ handleToDo(e.target.new.value)})
+})
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  let butt = document.createElement('button')
+  butt.addEventListener('click', handleDelete)
+  p.textContent = `${todo} `
+  butt.textContent = 'x'
+  p.appendChild(butt)
+
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+ console.log(e.target.parentNode.remove())
+}
